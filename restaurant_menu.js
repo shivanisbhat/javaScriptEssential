bda9bda292de318aad37e8fe8f67e53c6e656165
Shivani S Bhat <23cs297@mgits.ac.in>
2025-01-13 04:45:45 -0500

    message

--- restaurant_menu.js
+++ restaurant_menu.js
@@ -1,6 +1,7 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
     document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
